74d186f44d3536a01e7d4401dc0c6ab81fbbeb46
codewithdarkwa <jnrdrkw1@gmail.com>
2024-01-07 17:04:11 +0000

    update

--- lib/signup.dart
+++ lib/signup.dart
@@ -33,231 +33,176 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SingleChildScrollView(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Welcome to Supabase",
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Welcome to Supabase",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Required field missing';
+                        }
+                        return null;
+                      },
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your email',
+                        prefixIcon: const Icon(Icons.email, color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.all(12),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Required field missing';
+                        }
+                        return null;
+                      },
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your password',
+                        prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.all(12),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      controller: usernameController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Required field missing';
+                        }
+                        return null;
+                      },
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your username',
+                        prefixIcon: const Icon(Icons.email, color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        contentPadding: const EdgeInsets.all(12),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: GestureDetector(
+                      onTap: signUp,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 45,
+                        width: 360,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const Text(
+                          "Sign Up",
                           textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26)),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 16,
-                          top: 28,
-                          right: 16,
-                        ),
-                        child: Container(
-                          width: 343,
-                          decoration: BoxDecoration(
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.blue.shade50,
-                              width: 1,
-                            ),
-                          ),
-                          child: TextFormField(
-                            controller: emailController,
-                            decoration: InputDecoration(
-                                hintText: "Enter your email",
-                                hintStyle: TextStyle(fontSize: 12.0, color: Colors.blueGrey.shade300),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5),
-                                  borderSide: const BorderSide(color: Colors.white70),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5),
-                                  borderSide: const BorderSide(color: Colors.white70),
-                                ),
-                                prefixIcon: const Padding(
-                                  padding: EdgeInsets.all(
-                                    15,
-                                  ),
-                                  child: Icon(Icons.email_outlined),
-                                ),
-                                filled: true,
-                                fillColor: Colors.white),
-                            style: TextStyle(
-                                color: Colors.blueGrey.shade300,
-                                fontSize: 14.0,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 343,
-                        height: 48,
-                        margin: const EdgeInsets.only(
-                          left: 16,
-                          top: 8,
-                          right: 16,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white70,
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: Colors.blue.shade50,
-                            width: 1,
-                          ),
-                        ),
-                        child: TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              hintText: "Enter Password",
-                              hintStyle: TextStyle(fontSize: 12.0, color: Colors.blueGrey.shade300),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide: const BorderSide(color: Colors.white70),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide: const BorderSide(color: Colors.white70),
-                              ),
-                              prefixIcon: const Padding(
-                                padding: EdgeInsets.all(
-                                  15,
-                                ),
-                                child: Icon(Icons.lock_outline_rounded),
-                              ),
-                              filled: true,
-                              fillColor: Colors.white),
                           style: TextStyle(
-                              color: Colors.blueGrey.shade300,
-                              fontSize: 14.0,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w400),
-                        ),
-                      ),
-                      Container(
-                        width: 343,
-                        height: 48,
-                        margin: const EdgeInsets.only(
-                          left: 16,
-                          top: 8,
-                          right: 16,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white70,
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: Colors.blue.shade50,
-                            width: 1,
+                            color: Colors.white,
+                            fontSize: 20,
                           ),
                         ),
-                        child: TextFormField(
-                          controller: usernameController,
-                          decoration: InputDecoration(
-                              hintText: "Enter username",
-                              hintStyle: TextStyle(fontSize: 12.0, color: Colors.blueGrey.shade300),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide: const BorderSide(color: Colors.white70),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide: const BorderSide(color: Colors.white70),
-                              ),
-                              prefixIcon: const Padding(
-                                padding: EdgeInsets.all(
-                                  15,
-                                ),
-                                child: Icon(Icons.person),
-                              ),
-                              filled: true,
-                              fillColor: Colors.white),
-                          style: TextStyle(
-                              color: Colors.blueGrey.shade300,
-                              fontSize: 14.0,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w400),
-                        ),
                       ),
-                      GestureDetector(
-                        onTap: signUp,
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 16,
-                            top: 18,
-                            right: 16,
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: Colors.white,
-                                width: 1,
-                              ),
-                            ),
-                            alignment: Alignment.center,
-                            height: 57,
-                            width: 343,
-                            child: const Text(
-                              "Sign Up",
-                              textAlign: TextAlign.center,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+                    },
+                    child: Container(
+                      width: 212,
+                      margin: const EdgeInsets.only(left: 81, top: 8, right: 82),
+                      child: RichText(
+                        text: TextSpan(
+                          children: <InlineSpan>[
+                            TextSpan(
+                              text: "Already have an account?",
                               style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
+                                color: Colors.blueGrey.shade300,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
                               ),
                             ),
-                          ),
+                            TextSpan(
+                              text: ' ',
+                              style: TextStyle(
+                                  color: Colors.indigo.shade300,
+                                  fontSize: 12,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            TextSpan(
+                              text: "SignIn",
+                              style: TextStyle(
+                                  color: Colors.lightBlue.shade300,
+                                  fontSize: 12,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w700),
+                            )
+                          ],
                         ),
+                        textAlign: TextAlign.center,
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
-                        },
-                        child: Container(
-                          width: 212,
-                          margin: const EdgeInsets.only(
-                            left: 81,
-                            top: 8,
-                            right: 82,
-                          ),
-                          child: RichText(
-                              text: TextSpan(children: <InlineSpan>[
-                                TextSpan(
-                                  text: "Already have an account?",
-                                  style: TextStyle(
-                                      color: Colors.blueGrey.shade300,
-                                      fontSize: 12,
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.w400),
-                                ),
-                                TextSpan(
-                                  text: ' ',
-                                  style: TextStyle(
-                                      color: Colors.indigo.shade300,
-                                      fontSize: 12,
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.w700),
-                                ),
-                                TextSpan(
-                                  text: "SignIn",
-                                  style: TextStyle(
-                                      color: Colors.lightBlue.shade300,
-                                      fontSize: 12,
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.w700),
-                                )
-                              ]),
-                              textAlign: TextAlign.center),
-                        ),
-                      )
-                    ]),
-              )
-            ],
+                    ),
+                  )
+                ]),
           ),
         ),
       ),
