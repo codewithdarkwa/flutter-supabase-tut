a6355a038994f390bde0063bfbdd23cde7e645fe
codewithdarkwa <jnrdrkw1@gmail.com>
2024-01-09 20:47:28 +0000

    update

--- lib/login.dart
+++ lib/login.dart
@@ -27,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
 
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const Home()));
     } on AuthException catch (e) {
-      print(e);
+      debugPrint(e.message);
     }
   }
 
